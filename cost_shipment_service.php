ee97135b15a6bbe98fbc60699b8e2d24e2581e99
Brian Ramos <brianramos@gmail.com>
2017-04-20 17:51:56 +0200

    ok

--- cost_shipment_service.php
+++ cost_shipment_service.php
@@ -1,7 +1,12 @@
 <?php
 
 function costShipment($rexRateRequest) { 
-   return 'TEST';
+   return '<ns1:costDetails>
+				<ns1:costDetail>
+					<ns1:cost>666.00</ns1:cost>
+					<ns1:currencyCode>EUR</ns1:currencyCode>
+				</ns1:costDetail>
+			</ns1:costDetails>';
 }
  
    ini_set("soap.wsdl_cache_enabled", "0"); 
