c0364b4dc08d17f9d5428735c14149857cb00b8f
Brian Ramos <brianramos@gmail.com>
2017-04-20 17:47:36 +0200

    brr

--- cost_shipment_service.php
+++ cost_shipment_service.php
@@ -1,20 +1,7 @@
 <?php
 
 function costShipment($rexRateRequest) { 
-   return '<?xml version="1.0" encoding="UTF-8"?>
-				<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://xmlns.oracle.com/apps/otm/ExternalRating">
-					<SOAP-ENV:Body>
-						<env:Body>
-						  <ns1:rexRateResult>
-							<ns1:costDetails>
-								<ns1:costDetail>
-									<ns1:cost>666.00</ns1:cost>
-									<ns1:currencyCode>EUR</ns1:currencyCode>
-								</ns1:costDetail>
-							</ns1:costDetails>
-						  </ns1:rexRateResult>
-					</SOAP-ENV:Body>
-				</SOAP-ENV:Envelope>';
+   return 'TEST';
 }
  
    ini_set("soap.wsdl_cache_enabled", "0"); 
