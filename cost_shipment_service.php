3c71ad376e5c37efe9a499b5d635fe686776428f
Brian Ramos <brianramos@gmail.com>
2017-04-20 17:26:37 +0200

    updated cost_shipment_service with function costShipment

--- cost_shipment_service.php
+++ cost_shipment_service.php
@@ -1,3 +1,24 @@
 <?php
-  $server = new SoapServer("cost_shipment_service.wsdl");
+
+function costShipment($xml) { 
+   return '<?xml version="1.0" encoding="UTF-8"?>
+				<env:Envelope xmlns:env="http://schemas.xmlsoap.org/soap/envelope/">
+				  <env:Header/>
+					<env:Body>
+					  <rexRateResult>
+						<costDetails>
+							<costDetail>
+								<cost>666.00</cost>
+								<currencyCode>EUR</currencyCode>
+							</costDetail>
+						</costDetails>
+					  </rexRateResult>
+					</env:Body>
+				</env:Envelope>';
+}
+ 
+   ini_set("soap.wsdl_cache_enabled", "0"); 
+   $server = new SoapServer("cost_shipment_service.wsdl");
+   $server->addFunction("costShipment"); 
+   $server->handle(); 
 ?>
